83871ce3b1b13001ba8c482f416ae7ddc1d7e931
LuLuLaLaLa <haesung0728@gmail.com>
2023-08-30 21:57:51 +0900

    프로젝트 구성

---
+++ src/test/kotlin/com/sggnology/nightsaver/NightSaverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sggnology.nightsaver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NightSaverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
