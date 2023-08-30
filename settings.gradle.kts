4afafe68a64c6862cba9f0ffa9d3cf236ff4db6a
sggnology <sggnology@gmail.com>
2023-08-30 22:41:35 +0900

    Removed .idea files

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "night-saver"
--- src/main/kotlin/com/sggnology/nightsaver/db/sql/repository/FcmInfoRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sggnology.nightsaver.db.sql.repository
-
-import com.sggnology.nightsaver.db.sql.entity.FcmInfoEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface FcmInfoRepository: JpaRepository<FcmInfoEntity, Int> {
-}
