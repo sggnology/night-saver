e2b38930013ea1a3f1870a66c604bdac491c0e79
sggnology <sggnology@gmail.com>
2023-09-08 20:29:30 +0900

    [service] ahello, 예외 발생 테스트 코드 작성

--- src/main/kotlin/com/sggnology/nightsaver/application/AHello/controller/AHelloController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/AHello/controller/AHelloController.kt
@@ -2,6 +2,7 @@ package com.sggnology.nightsaver.application.AHello.controller
 
 import com.sggnology.nightsaver.component.push.message.dto.FcmMessageDto
 import com.sggnology.nightsaver.component.push.send.FcmSender
+import com.sggnology.nightsaver.exception.NotDefinedException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController
 class AHelloController(
     private val fcmSender: FcmSender
 ) {
+
+    @GetMapping("/exception")
+    fun exceptionTest(){
+        throw NotDefinedException()
+    }
 
     @GetMapping("/testSendNotification")
     fun testSendNotification(title: String){
