67728d055c0e0dae4bf0b9c19f73f64becbc0e8f
sggnology <sggnology@gmail.com>
2023-09-08 20:45:30 +0900

    [service] application, swagger 정보 설정

--- src/main/kotlin/com/sggnology/nightsaver/NightSaverApplication.kt
+++ src/main/kotlin/com/sggnology/nightsaver/NightSaverApplication.kt
@@ -1,9 +1,23 @@
 package com.sggnology.nightsaver
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = io.swagger.v3.oas.annotations.info.Info(
+        title = "Night Saver Server",
+        version = "0.0.1",
+        description = "Night Saver Server API Docs"
+    ),
+    servers = [
+        io.swagger.v3.oas.annotations.servers.Server(
+            url = "/",
+            description = "self host"
+        )
+    ]
+)
 class NightSaverApplication
 
 fun main(args: Array<String>) {
