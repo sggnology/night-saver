aea909f339b150b14a169d5312be7833e0db8065
sggnology <sggnology@gmail.com>
2023-09-08 20:52:12 +0900

    [service] token, swagger 설명 추가

--- src/main/kotlin/com/sggnology/nightsaver/server/push/token/TokenController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/push/token/TokenController.kt
@@ -2,6 +2,8 @@ package com.sggnology.nightsaver.server.push.token
 
 import com.sggnology.nightsaver.application.push.token.register.TokenRegisterService
 import com.sggnology.nightsaver.application.push.token.register.dto.TokenRegisterReqDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/push/token")
+@Tag(name = "토큰 API", description = "토큰 관리는 여기서 합니다.")
 class TokenController(
     private val tokenRegisterService: TokenRegisterService
 ) {
 
+    @Operation(summary = "토큰 등록")
     @PostMapping("")
     fun registerToken(
         @RequestBody tokenRegisterReqDto: TokenRegisterReqDto
