d73ad07992004e49e06a3c4a50ca091ce4f03e28
sggnology <sggnology@gmail.com>
2023-09-21 21:00:52 +0900

    [service] config, restTemplateConfig 에서 sub server 의 host 불러오는 @Value 패키지 수정

--- src/main/kotlin/com/sggnology/nightsaver/config/RestTemplateConfig.kt
+++ src/main/kotlin/com/sggnology/nightsaver/config/RestTemplateConfig.kt
@@ -1,16 +1,17 @@
 package com.sggnology.nightsaver.config
 
-import com.google.api.client.util.Value
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.DefaultUriBuilderFactory
 
 @Configuration
 class RestTemplateConfig {
 
-    @Value("\${sub.server.night-saver-image-server.url:http://localhost:8080}")
+    @Value("\${sub.server.night-saver-image-server.host:http://localhost:8080}")
     val nightSaverImageServerUrl: String = ""
 
     @Bean
