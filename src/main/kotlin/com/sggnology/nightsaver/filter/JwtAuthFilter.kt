7a88976b65c606f32e72fc4385f79b205e51fa64
haesungJoo <haesung0728@gmail.com>
2023-09-24 18:44:08 +0900

    [service] filter > auth, JwtAuthFilter 추가
    - jwt token 검증

---
+++ src/main/kotlin/com/sggnology/nightsaver/filter/JwtAuthFilter.kt
@@ -0,0 +1,71 @@
+package com.sggnology.nightsaver.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.sggnology.nightsaver.auth.JwtAuthProvider
+import com.sggnology.nightsaver.common.exception.BaseException
+import com.sggnology.nightsaver.common.response.ApiResult
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.GenericFilterBean
+
+@Component
+class JwtAuthFilter(
+    private val jwtAuthProvider: JwtAuthProvider
+): GenericFilterBean() {
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        val httpServletRequest = request as HttpServletRequest
+
+        val jwtToken = httpServletRequest.getHeader("Authorization")
+
+        if(jwtToken != null){
+
+            try{
+                jwtAuthProvider.validateToken(jwtToken)
+            }
+            catch (e: Exception){
+                exceptionHandle(response, e)
+                return
+            }
+
+            SecurityContextHolder.getContext().authentication = jwtAuthProvider.getAuthentication(jwtToken)
+        }
+
+        chain.doFilter(request, response)
+    }
+
+    /**
+     * 설명
+     * - filter chain 에서 발생한 오류는 우리가 정의한 ExceptionHandler 의 로직을 타지 않는다.
+     * - 따라서, filter chain 에서 발생한 오류를 처리하기 위한 로직이 필요하다.
+     * */
+    private fun exceptionHandle(response: ServletResponse, e: Exception){
+
+        var apiResult = ApiResult.fail(
+            errStatus = HttpStatus.UNAUTHORIZED,
+            errMsg = "인증되지 않은 사용자입니다."
+        )
+
+        if(e is BaseException){
+            logger.error(e.toApiResult())
+            apiResult = e.toApiResult()
+        }
+
+        val httpServletResponse = response as HttpServletResponse
+
+        httpServletResponse.status = 200
+        httpServletResponse.contentType = "application/json"
+
+        val outputStream = httpServletResponse.outputStream
+
+        outputStream.write(ObjectMapper().writeValueAsBytes(apiResult))
+        outputStream.close()
+    }
+}
