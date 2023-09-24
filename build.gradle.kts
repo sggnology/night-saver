0e1f0b5e0ed7da8990393c3ea7252c189fbbe608
haesungJoo <haesung0728@gmail.com>
2023-09-24 18:24:50 +0900

    [lib] jwt 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,11 @@ dependencies {
 
     // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    // jwt auth
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<KotlinCompile> {
