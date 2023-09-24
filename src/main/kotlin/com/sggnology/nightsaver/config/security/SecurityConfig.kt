0b1c497a8c80163106c948eeccb826114a4d1fc5
haesungJoo <haesung0728@gmail.com>
2023-09-24 18:49:24 +0900

    [service] config > security, SecurityConfig 특정 경로는 권한 확인 없이 허용

--- src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
@@ -3,10 +3,27 @@ package com.sggnology.nightsaver.config.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
+
+    private val commonExcludePathPatternList = listOf(
+        "/api/v1/hello/**", // 테스트 api
+        "/swagger-ui-resources/**",
+        "/swagger-ui/**",
+//        "/error",
+        "/api/admin/v2/login"
+    )
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring().requestMatchers(*commonExcludePathPatternList.toTypedArray())
+        }
+    }
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain{
