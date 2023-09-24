c0870d55df7b94386af19ff7107fee114e474a07
haesungJoo <haesung0728@gmail.com>
2023-09-24 18:51:54 +0900

    [service] config > security, SecurityConfig 세션 사용 및 `jwtAuthFilter` 를 filterChain 에 추가

--- src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
@@ -1,14 +1,19 @@
 package com.sggnology.nightsaver.config.security
 
+import com.sggnology.nightsaver.filter.JwtAuthFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthFilter: JwtAuthFilter
+) {
 
     private val commonExcludePathPatternList = listOf(
         "/api/v1/hello/**", // 테스트 api
@@ -29,7 +34,15 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain{
         http.csrf { it.disable() }
 
-        http.authorizeHttpRequests { it.anyRequest().permitAll() }
+        http.sessionManagement { sessionManagement ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }
+
+        http.authorizeHttpRequests {
+            it.anyRequest().permitAll()
+        }
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
