419c8acf05d815fca905d964209448d539f29f47
sggnology <sggnology@gmail.com>
2023-09-27 21:23:35 +0900

    [service] signup, 회원가입을 위한 요청 dto 구성

---
+++ src/main/kotlin/com/sggnology/nightsaver/application/signup/dto/req/SignupReqDto.kt
@@ -0,0 +1,13 @@
+package com.sggnology.nightsaver.application.signup.dto.req
+
+import jakarta.validation.constraints.NotEmpty
+
+data class SignupReqDto(
+    @field:NotEmpty(message = "아이디를 입력해주세요.")
+    val userId: String,
+    @field:NotEmpty(message = "비밀번호를 입력해주세요.")
+    val password: String,
+    val nickName: String? = null,
+    @field:NotEmpty(message = "검증 코드를 입력해주세요.")
+    val certificationText: String
+)
