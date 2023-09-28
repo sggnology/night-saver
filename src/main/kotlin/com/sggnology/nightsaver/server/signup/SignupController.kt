75ce82576223ef3218e5b1185851895b9043659c
sggnology <sggnology@gmail.com>
2023-09-28 21:30:06 +0900

    [service] server > signup, 회원가입 endpoint 에서 `인증코드`관련 endpoint 삭제

--- src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
@@ -1,7 +1,6 @@
 package com.sggnology.nightsaver.server.signup
 
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,13 +11,6 @@ class SignupController {
 
     @PostMapping("")
     fun signup(signupReqDto: SignupReqDto) {
-
-    }
-
-    @GetMapping("/certification-code")
-    fun requestCertificationCode(
-        userId: String
-    ) {
 
     }
 }
