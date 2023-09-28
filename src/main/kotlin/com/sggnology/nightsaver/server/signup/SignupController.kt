08b6f701acfdb24ce8f20254afe7c2cbd4b8572c
sggnology <sggnology@gmail.com>
2023-09-28 22:19:35 +0900

    [service] server > signup, swagger 문서 작성

--- src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
@@ -1,16 +1,23 @@
 package com.sggnology.nightsaver.server.signup
 
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/signup")
+@Tag(name = "회원가입", description = "회원가입 API")
 class SignupController {
 
+    @Operation(summary = "회원가입")
     @PostMapping("")
-    fun signup(signupReqDto: SignupReqDto) {
+    fun signup(
+        @RequestBody signupReqDto: SignupReqDto
+    ) {
 
     }
 }
