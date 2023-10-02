16d755868668faba015975ce77ace5eb01046532
sggnology <sggnology@gmail.com>
2023-10-03 01:11:59 +0900

    [application] email, EmailSender 구현
    - 도메인으로써 이메일 전송을 책임진다.

---
+++ src/main/kotlin/com/sggnology/nightsaver/application/email/EmailSender.kt
@@ -0,0 +1,25 @@
+package com.sggnology.nightsaver.application.email
+
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+/**
+ * 설명
+ * - spring-starter-mail 를 lib 추가하고, 적절한 프로퍼티를 등록하면 javaMailSender 가 bean 으로 등록된다.
+ *
+ * 특이사항
+ * - 그러나 compile 단계에서 왜인지 bean 으로 등록되어 있지 않다고 명시되어 있기에 아래와 같이
+ * - @suppress annotation 을 통해 warning 을 무시하도록 하였다.
+ * */
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
+@Service
+class EmailSender(
+    private val javaMailSender: JavaMailSender
+) {
+
+    private val emailGenerator = EmailGenerator()
+
+    fun send() {
+        javaMailSender.send(emailGenerator.generate())
+    }
+}
