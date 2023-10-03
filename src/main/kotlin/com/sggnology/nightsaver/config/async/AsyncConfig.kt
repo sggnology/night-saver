99446c251b9956e46b867973d69e7d25763d8590
sggnology <sggnology@gmail.com>
2023-10-03 13:43:25 +0900

    [service] config > async, email 스레드 풀 설정

---
+++ src/main/kotlin/com/sggnology/nightsaver/config/async/AsyncConfig.kt
@@ -0,0 +1,25 @@
+package com.sggnology.nightsaver.config.async
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+class AsyncConfig {
+
+    private val CORE_POOL_SIZE = 2
+    private val MAX_POOL_SIZE = 10
+    private val QUEUE_CAPACITY = 20
+
+    @Bean(name = ["EMAIL_HANDLER_TASK_EXECUTOR"])
+    fun emailHandlerTaskExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = CORE_POOL_SIZE
+        executor.maxPoolSize = MAX_POOL_SIZE
+        executor.setQueueCapacity(QUEUE_CAPACITY)
+        executor.setThreadNamePrefix("email-handler-")
+        executor.initialize()
+        return executor
+    }
+}
