2a33a80da14778bac80e60c279754733e601df13
sggnology <sggnology@gmail.com>
2023-10-05 00:15:07 +0900

    [service] signup, SignupService 에서 Signup 도메인 수정으로 인한 side effect 적용

--- src/main/kotlin/com/sggnology/nightsaver/application/signup/SignupService.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/signup/SignupService.kt
@@ -3,19 +3,21 @@ package com.sggnology.nightsaver.application.signup
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
 import com.sggnology.nightsaver.db.sql.repository.UserInfoRepository
 import jakarta.transaction.Transactional
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class SignupService(
-    private val userInfoRepository: UserInfoRepository
+    private val userInfoRepository: UserInfoRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     @Transactional
     fun signup(
         signupReqDto: SignupReqDto
     ) {
-        val signup = Signup(signupReqDto)
-        val newUser = signup.newUser()
+        val signup = Signup()
+        val newUser = signup.createNewUser(signupReqDto, passwordEncoder)
         userInfoRepository.save(newUser)
     }
 }
