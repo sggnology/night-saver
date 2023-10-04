94694defd508181d341df88bb7d8cc1719542cdb
sggnology <sggnology@gmail.com>
2023-10-05 00:13:25 +0900

    [service] config > security, PasswordEncoder 빈 등록

--- src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/sggnology/nightsaver/config/security/SecurityConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -45,5 +47,10 @@ class SecurityConfig(
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder{
+        return BCryptPasswordEncoder()
     }
 }
