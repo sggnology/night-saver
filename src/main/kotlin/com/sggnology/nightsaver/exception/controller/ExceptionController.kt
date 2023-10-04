01815be64fcc762e1f71fb077a6b50c3d0abbe6d
sggnology <sggnology@gmail.com>
2023-10-04 23:21:56 +0900

    [service] exception > controller, BindException 에 대한 예외처리

--- src/main/kotlin/com/sggnology/nightsaver/exception/controller/ExceptionController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/exception/controller/ExceptionController.kt
@@ -3,6 +3,9 @@ package com.sggnology.nightsaver.exception.controller
 import com.sggnology.nightsaver.common.exception.BaseException
 import com.sggnology.nightsaver.common.response.ApiResult
 import logger
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindException
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -17,6 +20,21 @@ class ExceptionController {
         logger.severe(e.message)
 
         return e.toApiResult()
+    }
+
+    @ExceptionHandler(BindException::class)
+    fun handleNotValidException(
+        e: BindException
+    ): ApiResult<Nothing> {
+
+        logger.severe("validation errors : ${e.fieldErrors}")
+
+        val defaultMessage = e.fieldError?.defaultMessage
+
+        return ApiResult.fail(
+            errStatus = HttpStatus.BAD_REQUEST,
+            errMsg = defaultMessage ?: "옳지 않은 요청입니다."
+        )
     }
 
     @ExceptionHandler(Exception::class)
