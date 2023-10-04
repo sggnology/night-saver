f9cabfbc4d4610a99434dc9b183f63f7e09a6689
sggnology <sggnology@gmail.com>
2023-10-04 23:45:46 +0900

    [service] server > signup, 회원가입 요청 dto @Valid 처리

--- src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
@@ -3,6 +3,7 @@ package com.sggnology.nightsaver.server.signup
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +17,7 @@ class SignupController {
     @Operation(summary = "회원가입")
     @PostMapping("")
     fun signup(
-        @RequestBody signupReqDto: SignupReqDto
+        @Valid @RequestBody signupReqDto: SignupReqDto
     ) {
 
     }
