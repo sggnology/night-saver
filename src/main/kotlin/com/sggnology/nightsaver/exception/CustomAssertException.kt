a0478836ae50d1d86d4e8a7f343e2b875da4508f
sggnology <sggnology@gmail.com>
2023-10-04 23:21:37 +0900

    [service] exception > CustomAssertException, 기본 오류 메세지 오타 수정

--- src/main/kotlin/com/sggnology/nightsaver/exception/CustomAssertException.kt
+++ src/main/kotlin/com/sggnology/nightsaver/exception/CustomAssertException.kt
@@ -6,5 +6,5 @@ import org.springframework.http.HttpStatusCode
 
 class CustomAssertException(
     override val status: HttpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
-    errMsg: String = "요청하신 정보가 검증되지 않은 조건이 포함되어있ㅅ브니다."
+    errMsg: String = "요청하신 정보가 검증되지 않은 조건이 포함되어있습니다."
 ): BaseException(errMsg)
