a4a247d592542803f5b269b884e5c46e8f73a594
sggnology <sggnology@gmail.com>
2023-10-04 23:48:05 +0900

    [service] signup > dto, 회원가입 요청 dto 에 passwordConfirm 필드 추가

--- src/main/kotlin/com/sggnology/nightsaver/application/signup/dto/req/SignupReqDto.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/signup/dto/req/SignupReqDto.kt
@@ -7,6 +7,8 @@ data class SignupReqDto(
     val userEmail: String,
     @field:NotEmpty(message = "비밀번호를 입력해주세요.")
     val password: String,
+    @field:NotEmpty(message = "확인 비밀번호를 입력해주세요.")
+    val passwordConfirm: String,
     val nickName: String? = null,
     @field:NotEmpty(message = "검증 코드를 입력해주세요.")
     val certificationText: String
