a91c283768b8030debbeebd6a8ad1f19fe0242c2
sggnology <sggnology@gmail.com>
2023-10-05 00:14:41 +0900

    [service] signup, Signup 도메인 생성자 구조 수정
    - 새로운 유저 생성시 파라미터 제공하게끔 수정

--- src/main/kotlin/com/sggnology/nightsaver/application/signup/Signup.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/signup/Signup.kt
@@ -2,15 +2,14 @@ package com.sggnology.nightsaver.application.signup
 
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
 import com.sggnology.nightsaver.db.sql.entity.UserInfoEntity
+import org.springframework.security.crypto.password.PasswordEncoder
 
-class Signup(
-    private val signupReqDto: SignupReqDto
-) {
+class Signup {
 
-    fun newUser(): UserInfoEntity {
+    fun createNewUser(signupReqDto: SignupReqDto, passwordEncoder: PasswordEncoder): UserInfoEntity {
         return UserInfoEntity().apply {
             this.userEmail = signupReqDto.userEmail
-            this.userPw = signupReqDto.password
+            this.userPw = passwordEncoder.encode(signupReqDto.password)
             this.userNick = signupReqDto.nickName
         }
     }
