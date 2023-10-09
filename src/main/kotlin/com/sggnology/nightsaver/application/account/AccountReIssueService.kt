691d9c89f7cfd14c7489ab3a36519f089e4ff945
sggnology <sggnology@gmail.com>
2023-10-09 19:17:48 +0900

    [service] lost password, 비밀번호 잃어버린 상황에서 재발급 구현

---
+++ src/main/kotlin/com/sggnology/nightsaver/application/account/AccountReIssueService.kt
@@ -0,0 +1,30 @@
+package com.sggnology.nightsaver.application.account
+
+import com.sggnology.nightsaver.component.email.EmailSender
+import com.sggnology.nightsaver.component.email.generate.EmailGeneratorUsingViewFactory
+import com.sggnology.nightsaver.util.code.CodeGenerator
+import org.springframework.stereotype.Service
+
+@Service
+class AccountReIssueService(
+    private val emailSender: EmailSender,
+    private val accountChangePasswordService: AccountChangePasswordService
+) {
+
+    fun reIssue(
+        userEmail: String
+    ) {
+        val newPassword = CodeGenerator.generate(8, CodeGenerator.Companion.CodeType.ALPHANUMERIC)
+
+        emailSender.send(
+            emailGeneratorType = EmailGeneratorUsingViewFactory.Companion.EmailGeneratorType.ACCOUNT_RE_ISSUE_LOST_PASSWORD,
+            to = userEmail,
+            title = "NightSaver 비밀번호를 재발급 하였습니다.",
+            variableMap = mapOf(
+                "newPassword" to newPassword
+            )
+        )
+
+        accountChangePasswordService.change(userEmail, newPassword)
+    }
+}
--- src/main/kotlin/com/sggnology/nightsaver/server/account/AccountController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/account/AccountController.kt
@@ -1,18 +1,24 @@
 package com.sggnology.nightsaver.server.account
 
 import com.sggnology.nightsaver.application.account.AccountDuplicationService
+import com.sggnology.nightsaver.application.account.AccountReIssueService
 import com.sggnology.nightsaver.common.response.ApiResult
+import com.sggnology.nightsaver.db.sql.repository.UserPwLostLogInfoRepository
+import com.sggnology.nightsaver.extension.customAssert
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/v1/account")
 @Tag(name = "계정", description = "계정 관련 API")
 class AccountController(
-    private val accountDuplicationService: AccountDuplicationService
+    private val accountDuplicationService: AccountDuplicationService,
+    private val accountReIssueService: AccountReIssueService,
+    private val userPwLostLogInfoRepository: UserPwLostLogInfoRepository
 ) {
 
     @Operation(summary = "아이디 중복 검사")
@@ -23,5 +29,17 @@ class AccountController(
         return ApiResult.success(
             accountDuplicationService.isUserEmailDuplicated(userEmail)
         )
+    }
+
+    @Operation(summary = "잃어버린 비밀번호 재발급(로그인 안한 상태)")
+    @PostMapping("/lost/password")
+    fun reIssueLostPassword(
+        userEmail: String
+    ): ApiResult<Nothing> {
+        val userPwLostLogInfoEntity = userPwLostLogInfoRepository.findFirstByUserEmailOrderByCreatedAtDesc(userEmail)
+        customAssert(userPwLostLogInfoEntity?.createdAt?.plusMinutes(10)?.isAfter(LocalDateTime.now()) == true, "비밀번호 재발급을 위한 인증 과정을 진행하여주세요.")
+        customAssert(userPwLostLogInfoEntity?.certificatedYn == "Y", "비밀번호 재발급 코드를 인증하여주세요.")
+        accountReIssueService.reIssue(userEmail)
+        return ApiResult.success()
     }
 }
