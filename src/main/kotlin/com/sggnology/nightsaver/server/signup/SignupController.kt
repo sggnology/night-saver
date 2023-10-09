939c7a05ed21c4fd179f9415b8b701e0b423ff42
sggnology <sggnology@gmail.com>
2023-10-09 19:09:07 +0900

    [service] server > signup, 회원가입 성공시 기본 성공 응답 반응하게끔 수정

--- src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/signup/SignupController.kt
@@ -1,9 +1,9 @@
 package com.sggnology.nightsaver.server.signup
 
 import com.sggnology.nightsaver.application.account.AccountDuplicationService
-import com.sggnology.nightsaver.application.certification.CertificateSignupService
 import com.sggnology.nightsaver.application.signup.SignupService
 import com.sggnology.nightsaver.application.signup.dto.req.SignupReqDto
+import com.sggnology.nightsaver.common.response.ApiResult
 import com.sggnology.nightsaver.extension.customAssert
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -25,9 +25,10 @@ class SignupController(
     @PostMapping("")
     fun signup(
         @Valid @RequestBody signupReqDto: SignupReqDto
-    ) {
+    ): ApiResult<Nothing> {
         customAssert(signupReqDto.password == signupReqDto.passwordConfirm, "비밀번호가 일치하지 않습니다.")
         customAssert(!accountDuplicationService.isUserNickDuplicated(signupReqDto.userEmail), "이미 존재하는 닉네임입니다.")
         signupService.signup(signupReqDto)
+        return ApiResult.success()
     }
 }
