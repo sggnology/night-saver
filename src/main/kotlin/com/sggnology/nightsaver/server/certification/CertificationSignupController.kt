29ebdf3f849b8ad768107175c02907a786720d7a
sggnology <sggnology@gmail.com>
2023-10-09 19:08:36 +0900

    [service] server > certification, signup endpoint 분기
    - api/v1/certification 내부에서 여러 요청을 처리하려 하였으나 비대해질 듯 하여 signup 을 위한 요청처리를 분기하였다.

--- src/main/kotlin/com/sggnology/nightsaver/server/certification/CertificationSignupController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/certification/CertificationSignupController.kt
@@ -2,6 +2,7 @@ package com.sggnology.nightsaver.server.certification
 
 import com.sggnology.nightsaver.application.certification.CertificateSignupService
 import com.sggnology.nightsaver.application.signup.dto.req.SignupCertificationReqDto
+import com.sggnology.nightsaver.common.response.ApiResult
 import com.sggnology.nightsaver.extension.customAssert
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -10,27 +11,29 @@ import logger
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/certification")
+@RequestMapping("/api/v1/certification/signup")
 @Tag(name = "인증", description = "인증 관련 API")
-class CertificationController(
+class CertificationSignupController(
     private val certificateSignupService: CertificateSignupService
 ) {
 
     @Operation(summary = "회원가입 인증 코드 요청")
-    @GetMapping("/signup")
+    @GetMapping("")
     fun requestCertificationCode(
         userEmail: String
-    ) {
+    ): ApiResult<Nothing> {
         certificateSignupService.sendCodeTo(userEmail)
         logger.info("mail : $userEmail, code : ${certificateSignupService.getCode(userEmail)}, 회원가입 인증 코드 전송 완료")
+        return ApiResult.success()
     }
 
     @Operation(summary = "회원가입 인증 코드 검증")
-    @PostMapping("/signup")
+    @PostMapping("")
     fun certificateCode(
         @Valid @RequestBody signupCertificationReqDto: SignupCertificationReqDto
-    ) {
+    ): ApiResult<Nothing> {
         val code = certificateSignupService.getCode(signupCertificationReqDto.userEmail)
         customAssert(code == signupCertificationReqDto.certificationCode, "인증 코드가 일치하지 않습니다.")
+        return ApiResult.success()
     }
 }
