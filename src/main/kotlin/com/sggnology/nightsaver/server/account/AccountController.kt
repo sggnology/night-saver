3c24c4057cfc8ad286208454697392fd01e89e6f
sggnology <sggnology@gmail.com>
2023-10-15 21:50:13 +0900

    [service] lost password, 잃어버린 비밀번호 재발급 위한 요청 dto 구성

--- src/main/kotlin/com/sggnology/nightsaver/server/account/AccountController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/account/AccountController.kt
@@ -2,12 +2,15 @@ package com.sggnology.nightsaver.server.account
 
 import com.sggnology.nightsaver.application.account.AccountDuplicationService
 import com.sggnology.nightsaver.application.account.AccountReIssueService
+import com.sggnology.nightsaver.application.account.dto.ReIssueLostPasswordReqDto
 import com.sggnology.nightsaver.common.response.ApiResult
 import com.sggnology.nightsaver.db.sql.repository.UserPwLostLogInfoRepository
 import com.sggnology.nightsaver.extension.customAssert
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
@@ -34,12 +37,12 @@ class AccountController(
     @Operation(summary = "잃어버린 비밀번호 재발급(로그인 안한 상태)")
     @PostMapping("/lost/password")
     fun reIssueLostPassword(
-        userEmail: String
+        @Valid @RequestBody reIssueLostPasswordReqDto: ReIssueLostPasswordReqDto
     ): ApiResult<Nothing> {
-        val userPwLostLogInfoEntity = userPwLostLogInfoRepository.findFirstByUserEmailOrderByCreatedAtDesc(userEmail)
+        val userPwLostLogInfoEntity = userPwLostLogInfoRepository.findFirstByUserEmailOrderByCreatedAtDesc(reIssueLostPasswordReqDto.userEmail)
         customAssert(userPwLostLogInfoEntity?.createdAt?.plusMinutes(10)?.isAfter(LocalDateTime.now()) == true, "비밀번호 재발급을 위한 인증 과정을 진행하여주세요.")
         customAssert(userPwLostLogInfoEntity?.certificatedYn == "Y", "비밀번호 재발급 코드를 인증하여주세요.")
-        accountReIssueService.reIssue(userEmail)
+        accountReIssueService.reIssue(reIssueLostPasswordReqDto)
         return ApiResult.success()
     }
 }
