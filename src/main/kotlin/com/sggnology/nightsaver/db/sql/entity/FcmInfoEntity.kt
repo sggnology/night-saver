069694e2a9cf1901de48c92d64b0c09d233a51a6
sggnology <sggnology@gmail.com>
2023-10-17 01:05:07 +0900

    [service] db > fcmInfo, userId 추가 및 userinfo 와의 연관관계 맵핑

--- src/main/kotlin/com/sggnology/nightsaver/db/sql/entity/FcmInfoEntity.kt
+++ src/main/kotlin/com/sggnology/nightsaver/db/sql/entity/FcmInfoEntity.kt
@@ -1,26 +1,33 @@
 package com.sggnology.nightsaver.db.sql.entity
 
 import jakarta.persistence.*
-import java.sql.Timestamp
 import java.time.LocalDateTime
 
 @Entity
 @Table(name = "fcm_info", schema = "night-saver", catalog = "")
-class FcmInfoEntity (
+class FcmInfoEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "idx")
-    val idx: Int = 0,
+    val idx: Int = 0
+
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    var userId: Int = 0
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false, insertable = false, updatable = false)
+    lateinit var userInfoEntity: UserInfoEntity
 
     @Basic
     @Column(name = "fcm_token", nullable = false, length = 255)
-    val fcmToken: String,
+    lateinit var fcmToken: String
 
     @Basic
     @Column(name = "created_at", nullable = false)
-    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val createdAt: LocalDateTime = LocalDateTime.now()
 
     @Basic
     @Column(name = "updated_at", nullable = false)
     val updatedAt: LocalDateTime = LocalDateTime.now()
-)
+}
