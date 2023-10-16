c30f4c5e2a32faa37caeddf80b0a2e29c24e8530
sggnology <sggnology@gmail.com>
2023-10-17 00:42:39 +0900

    [service] resolver > userinfo, UserInfo의 불필요한 annotation 삭제

--- src/main/kotlin/com/sggnology/nightsaver/resolver/annotation/UserInfo.kt
+++ src/main/kotlin/com/sggnology/nightsaver/resolver/annotation/UserInfo.kt
@@ -5,5 +5,4 @@ import io.swagger.v3.oas.annotations.Parameter
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 @Parameter(hidden = true)
-annotation class UserInfo {
-}
+annotation class UserInfo
