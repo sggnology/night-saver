e376f9c2aa13f6069d8aadbbb7276f3c2db72f00
sggnology <sggnology@gmail.com>
2023-10-19 21:33:08 +0900

    [service] db, 사용하지 않는 user_signup_status_info 테이블 삭제

--- src/main/kotlin/com/sggnology/nightsaver/db/sql/repository/UserSignupStatusInfoRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.sggnology.nightsaver.db.sql.repository
-
-import com.sggnology.nightsaver.db.sql.entity.UserInfoEntity
-import com.sggnology.nightsaver.db.sql.entity.UserSignupStatusInfoEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserSignupStatusInfoRepository: JpaRepository<UserSignupStatusInfoEntity, Int> {
-}
