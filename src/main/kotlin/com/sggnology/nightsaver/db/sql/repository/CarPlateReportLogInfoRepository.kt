d4ad760bc42b29d95ba6268e4edd1fd84bea0ebe
sggnology <sggnology@gmail.com>
2023-10-23 00:08:44 +0900

    [service] db > CarPlateReportLogInfo, createdAt 기준으로 마지막 값을 조회하기 위한 jpql 추가
    - n+1 이슈를 발생시키지 않기 위해 sql 따로 선언

--- src/main/kotlin/com/sggnology/nightsaver/db/sql/repository/CarPlateReportLogInfoRepository.kt
+++ src/main/kotlin/com/sggnology/nightsaver/db/sql/repository/CarPlateReportLogInfoRepository.kt
@@ -2,6 +2,13 @@ package com.sggnology.nightsaver.db.sql.repository
 
 import com.sggnology.nightsaver.db.sql.entity.CarPlateReportLogInfoEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CarPlateReportLogInfoRepository: JpaRepository<CarPlateReportLogInfoEntity, Int> {
+
+    @Query("""
+        select cprli from CarPlateReportLogInfoEntity cprli
+        order by cprli.createdAt desc limit 1
+    """)
+    fun getLastCarPlateReportLog(): CarPlateReportLogInfoEntity?
 }
