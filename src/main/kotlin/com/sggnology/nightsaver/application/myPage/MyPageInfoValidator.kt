363204417ebce35483c60cc507729ea6a308f0fa
sggnology <sggnology@gmail.com>
2023-10-26 23:50:09 +0900

    [service] mypage, MyPageInfoValidator 설명 주석 작성

--- src/main/kotlin/com/sggnology/nightsaver/application/myPage/MyPageInfoValidator.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/myPage/MyPageInfoValidator.kt
@@ -11,6 +11,18 @@ class MyPageInfoValidator<T>(
     private val userSqlFunc: KFunction<UserInfoEntity?>
 ) {
 
+    /**
+     * 설명
+     * - 중복이 허용되지 않는 값에 대한 검증
+     *
+     * 상세설명
+     * - `targetField 가 null` 이면 null 이 허용되는 필드에 대한 요청임으로 검증을 통과시킨다.
+     * - `existUser == null` 일 경우 targetField 는 중복되지 않은 값임으로 검증을 통과시킨다.
+     * - `user == existUser` 일 경우 targetField 의 값이 본인의 값 임으로 중복되지 않은 경우로 검증을 통과시킨다.
+     *
+     * 특이사항
+     * - unique 한 필드에 대해서만 사용 가능하다.
+     * */
     fun validate(): Boolean{
 
         if(targetField == null){
@@ -19,6 +31,6 @@ class MyPageInfoValidator<T>(
 
         val existUser = userSqlFunc.call(userSqlInstance, targetField)
 
-        return user == existUser || existUser == null
+        return existUser == null || user == existUser
     }
 }
