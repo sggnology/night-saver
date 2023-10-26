03b9c89395353cf8b8d2d930b30f37448bc84507
sggnology <sggnology@gmail.com>
2023-10-26 23:39:00 +0900

    [service] signup > dto, 회원가입시 `carPlateNumber` 필드 추가

--- src/main/kotlin/com/sggnology/nightsaver/application/signup/dto/req/SignupReqDto.kt
+++ src/main/kotlin/com/sggnology/nightsaver/application/signup/dto/req/SignupReqDto.kt
@@ -13,4 +13,6 @@ data class SignupReqDto(
     val passwordConfirm: String,
     @field:Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 한글, 영문, 숫자로 2~10자리로 입력해주세요.")
     val nickName: String? = null,
+    @field:Pattern(regexp = "^[0-9]{2,3}[가-힣]{1}[0-9]{4}$", message = "차량번호 형식이 올바르지 않습니다.")
+    val carPlateNumber: String,
 )
