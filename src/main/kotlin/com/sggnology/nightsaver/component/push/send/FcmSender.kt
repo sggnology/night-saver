ba10fcf03664627b50dd5a3a96a590688042e664
sggnology <sggnology@gmail.com>
2023-10-29 19:36:20 +0900

    [service] push, FcmSender 의 listener 삭제

--- src/main/kotlin/com/sggnology/nightsaver/component/push/send/FcmSender.kt
+++ src/main/kotlin/com/sggnology/nightsaver/component/push/send/FcmSender.kt
@@ -4,7 +4,6 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.sggnology.nightsaver.component.push.message.FcmMessageGenerator
 import com.sggnology.nightsaver.component.push.message.dto.FcmMessageDto
 import org.springframework.stereotype.Service
-import java.util.concurrent.Executors
 
 
 @Service
@@ -18,13 +17,6 @@ class FcmSender {
         val fcmMessagingInstance = FirebaseMessaging.getInstance()
         val multicastMessage = FcmMessageGenerator.generateMulticastMessage(fcmMessageDto)
 
-
-        fcmMessagingInstance.sendEachForMulticastAsync(multicastMessage).addListener(
-            {
-                // FIXME : fcm 전송 여부 logging 처리 되면 그때 처리
-                println("FCM send success")
-            },
-            Executors.newFixedThreadPool(10)
-        )
+        fcmMessagingInstance.sendEachForMulticastAsync(multicastMessage)
     }
 }
