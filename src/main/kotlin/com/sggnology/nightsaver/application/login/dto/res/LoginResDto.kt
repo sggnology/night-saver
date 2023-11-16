01f0a351286b0c186de9e3bdb9bb152a87ad6b41
sggnology <sggnology@gmail.com>
2023-11-16 22:10:42 +0900

    [service] login, 로그인 성공시 refresh token 도 응답하게끔 수정

---
+++ src/main/kotlin/com/sggnology/nightsaver/application/login/dto/res/LoginResDto.kt
@@ -0,0 +1,6 @@
+package com.sggnology.nightsaver.application.login.dto.res
+
+data class LoginResDto(
+    val accessToken: String,
+    val refreshToken: String
+)
--- src/main/kotlin/com/sggnology/nightsaver/server/login/LoginController.kt
+++ src/main/kotlin/com/sggnology/nightsaver/server/login/LoginController.kt
@@ -1,6 +1,7 @@
 package com.sggnology.nightsaver.server.login
 
 import com.sggnology.nightsaver.application.login.dto.req.LoginReqDto
+import com.sggnology.nightsaver.application.login.dto.res.LoginResDto
 import com.sggnology.nightsaver.auth.JwtAuthProvider
 import com.sggnology.nightsaver.common.response.ApiResult
 import com.sggnology.nightsaver.db.sql.repository.UserInfoRepository
@@ -27,12 +28,17 @@ class LoginController(
     @PostMapping("")
     fun login(
         @Valid @RequestBody loginReqDto: LoginReqDto
-    ): ApiResult<String> {
+    ): ApiResult<LoginResDto> {
         val user = userInfoRepository.findByUserEmail(loginReqDto.userEmail)
 
         customAssert(user != null, "존재하지 않는 계정입니다.")
         customAssert(passwordEncoder.matches(loginReqDto.password, user!!.userPw), "비밀번호가 일치하지 않습니다.")
 
-        return ApiResult.success(jwtAuthProvider.createAccessToken(user.userId))
+        return ApiResult.success(
+            LoginResDto(
+                accessToken = jwtAuthProvider.createAccessToken(user.userId),
+                refreshToken = jwtAuthProvider.createRefreshToken(user.userId)
+            )
+        )
     }
 }
