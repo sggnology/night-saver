a06c5cd64dc2e8d4217fae082f4d38d11a713cf0
sggnology <sggnology@gmail.com>
2023-11-16 22:05:26 +0900

    [service] auth > JwtAuthProvider, access/refresh token 각각 관심사 분리해서 선언
    - 추후 분리 예정

--- src/main/kotlin/com/sggnology/nightsaver/auth/JwtAuthProvider.kt
+++ src/main/kotlin/com/sggnology/nightsaver/auth/JwtAuthProvider.kt
@@ -9,6 +9,7 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.UnsupportedJwtException
 import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.security.SignatureException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
@@ -23,27 +24,32 @@ import org.springframework.security.core.userdetails.User
 @Service
 class JwtAuthProvider {
 
-    @Value("\${auth.jwt.secret-key}")
-    private val jwtSecretKey: String = ""
+    @Value("\${auth.jwt.access-token.secret-key}")
+    private val accessTokenSecretKey: String = ""
 
-    @Value("\${auth.jwt.expire-seconds}")
-    private val ACCESS_TOKEN_EXPIRE_SECONDS: Int = 1000 * 60 * 60 * 24
+    @Value("\${auth.jwt.refresh-token.secret-key}")
+    private val refreshTokenSecretKey: String = ""
 
-    @Value("\${auth.jwt.expire-seconds}")
-    private val REFRESH_TOKEN_EXPIRE_SECONDS: Int = 1000 * 60 * 60 * 24 * 30
+    @Value("\${auth.jwt.access-token.expire-seconds}")
+    private val ACCESS_TOKEN_EXPIRE_SECONDS: Int = 60 * 60 * 24
 
-    private lateinit var jwtSecret: SecretKey
+    @Value("\${auth.jwt.refresh-token.expire-seconds}")
+    private val REFRESH_TOKEN_EXPIRE_SECONDS: Int = 60 * 60 * 24 * 30
+
+    private lateinit var accessTokenSecret: SecretKey
+    private lateinit var refreshTokenSecret: SecretKey
 
     @PostConstruct
     fun init() {
-        jwtSecret = Keys.hmacShaKeyFor(jwtSecretKey.toByteArray())
+        accessTokenSecret = Keys.hmacShaKeyFor(accessTokenSecretKey.toByteArray())
+        refreshTokenSecret = Keys.hmacShaKeyFor(refreshTokenSecretKey.toByteArray())
     }
 
     fun createAccessToken(userId: Int): String {
         return Jwts.builder()
             .setSubject(userId.toString())
             .setExpiration(Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_SECONDS))
-            .signWith(jwtSecret)
+            .signWith(accessTokenSecret)
             .compact()
     }
 
@@ -51,12 +57,12 @@ class JwtAuthProvider {
         return Jwts.builder()
             .setSubject(userId.toString())
             .setExpiration(Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRE_SECONDS))
-            .signWith(jwtSecret)
+            .signWith(refreshTokenSecret)
             .compact()
     }
 
     fun getAuthentication(accessToken: String): Authentication {
-        val claims = Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(accessToken).body
+        val claims = Jwts.parserBuilder().setSigningKey(accessTokenSecret).build().parseClaimsJws(accessToken).body
         val userId = claims.subject
 
         val user = User(userId, "", listOf(getUserRole()))
@@ -64,13 +70,13 @@ class JwtAuthProvider {
         return UsernamePasswordAuthenticationToken(user, null, user.authorities)
     }
 
-    private fun getUserRole(): GrantedAuthority{
+    private fun getUserRole(): GrantedAuthority {
         return SimpleGrantedAuthority("USER")
     }
 
-    fun getUserIdFromJwtToken(token: String): Int {
+    fun getUserIdFromAccessToken(token: String): Int {
         return Jwts.parserBuilder()
-            .setSigningKey(jwtSecret)
+            .setSigningKey(accessTokenSecret)
             .build()
             .parseClaimsJws(token)
             .body
@@ -78,9 +84,19 @@ class JwtAuthProvider {
             .toInt()
     }
 
-    fun validateToken(token: String) {
+    fun getUserIdFromRefreshToken(token: String): Int {
+        return Jwts.parserBuilder()
+            .setSigningKey(refreshTokenSecret)
+            .build()
+            .parseClaimsJws(token)
+            .body
+            .subject
+            .toInt()
+    }
+
+    fun validateAccessToken(accessToken: String) {
         try {
-            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).body
+            Jwts.parserBuilder().setSigningKey(accessTokenSecret).build().parseClaimsJws(accessToken).body
         } catch (e: SecurityException) {
             // 잘못된 JWT 서명입니다.
             throw AuthWrongJwtSigningException()
@@ -103,5 +119,33 @@ class JwtAuthProvider {
             // JWT 토큰이 잘못되었습니다.
             throw AuthIllegalJwtException()
         }
+    }
+
+    fun validateRefreshToken(refreshToken: String) {
+        try {
+            Jwts.parserBuilder().setSigningKey(refreshTokenSecret).build().parseClaimsJws(refreshToken).body
+        } catch (e: SecurityException) {
+            // 잘못된 JWT 서명입니다.
+            throw AuthWrongJwtSigningException()
+        } catch (e: MalformedJwtException) {
+            // 잘못된 JWT 서명입니다.
+            throw AuthWrongJwtSigningException()
+        } catch (e: SignatureException) {
+            // 잘못된 JWT 서명입니다.
+            throw AuthWrongJwtSigningException()
+        } catch (e: ExpiredJwtException) {
+            // 만료된 JWT 토큰입니다.
+            throw AuthExpiredJwtException()
+        } catch (e: UnsupportedJwtException) {
+            // 지원되지 않는 JWT 토큰입니다.
+            throw AuthUnsupportedJwtException()
+        } catch (e: IllegalArgumentException) {
+            // JWT 토큰이 잘못되었습니다.
+            throw AuthIllegalJwtException()
+        } catch (e: Exception) {
+            // JWT 토큰이 잘못되었습니다.
+            throw AuthIllegalJwtException()
+        }
+
     }
 }
